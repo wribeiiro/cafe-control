8703251cbcfef20c6fa35a30a8e02ea521eaea86
Rafael Soje <rafaelsoje@gmail.com>
2020-07-30 00:03:14 -0300

    Erro de conexão e manutenção

--- source/App/Web.php
+++ source/App/Web.php
@@ -9,6 +9,7 @@
 namespace Source\App;
 
 
+use Source\Core\Connect;
 use Source\Core\Controller;
 use Source\Support\Pager;
 
@@ -216,17 +217,38 @@ class Web extends Controller
 	public function error(array $data): void
 	{
 		$error = new \stdClass();
-		$error->code = $data['errcode'];
-		$error->title = "Ooops. Conteúdo indisponível :/";
-		$error->message = "Sentimos muito,mas o conteúdo que vc está tentando ecessar não existe, está indisponível no momento ou foi removido :/";
-		$error->linkTitle = "Continue navegando!";
-		$error->link = url_back();
+		
+		switch ($data['errcode']){
+			case "problemas":
+				$error->code = "OPS";
+				$error->title = "Estamos enfrentando problemas";
+				$error->message = "Parece que nosso serviço não está disponível no momento. Já estamos vendo isso mas caso precise, envie um email :)";
+				$error->linkTitle = "ENVIAR E-MAIL";
+				$error->link = "mailto:". CONF_MAIL_SUPPORT;
+				break;
+				
+			case "manutencao":
+				$error->code = "OPS";
+				$error->title = "Desculpe. Estamos em manutenção!";
+				$error->message = "Voltamos logo! Por hora estamos trabalhando para melhorar nosso conteúdo para você controlar melhor suas contas :p";
+				$error->linkTitle = null;
+				$error->link = null;
+				break;
+				
+			default:
+				$error->code = $data['errcode'];
+				$error->title = "Ooops. Conteúdo indisponível :/";
+				$error->message = "Sentimos muito,mas o conteúdo que vc está tentando ecessar não existe, está indisponível no momento ou foi removido :/";
+				$error->linkTitle = "Continue navegando!";
+				$error->link = url_back();
+				break;
+		}
 		
 		$head = $this->seo->render(
 			"{$error->code} | {$error->title}",
 			$error->message,
-			url_back("/ops/{$error->code}"),
-			url("/assets/imagens/share.jpg"),
+			url("/ops/{$error->code}"),
+			theme("/assets/imagens/share.jpg"),
 			false
 		);
 		
